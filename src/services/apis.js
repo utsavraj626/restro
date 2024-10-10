11de870536d72c5a8416b45bf74de63206321c3b
Utsav Raj <140430251+utsavraj626@users.noreply.github.com>
2024-10-10 15:51:09 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 // const BASE_URL = process.env.REACT_APP_BASE_URL;
 // const BASE_URL = "http://localhost:4000";
-const BASE_URL = "https://restro-app-vese.onrender.com";
+const BASE_URL = "https://restro-app-yw5t.onrender.com";
 
 // AUTH ENDPOINTS
 export const endpoints = {
